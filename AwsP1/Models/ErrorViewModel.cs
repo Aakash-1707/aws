fa4f872e37096fe4d4fbd0796f1e4320ed8508f3
Joel981 <75721103+Joel981@users.noreply.github.com>
2022-08-26 19:41:20 +0530

    Add project files.

---
+++ AwsP1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AwsP1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
